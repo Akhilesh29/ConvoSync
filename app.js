bbcfe9da7e04d80adf58982affbc37fdf9943a75
Akhilesh Meena <98381403+Akhilesh29@users.noreply.github.com>
2024-05-30 22:35:29 +0530

    Update app.js

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ var io = require('socket.io').listen(server);
 io.sockets.on('connection', socket);
 
 /* Start server */
-server.listen(app.get('port'), function (){
+server.listen(app.get('port'), function(){
   console.log('Express server listening on port %d in %s mode', app.get('port'), app.get('env'));
 });
 
